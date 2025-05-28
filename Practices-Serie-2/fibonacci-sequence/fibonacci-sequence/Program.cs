ac71453215fc18a5e58ce52b3ae80b53fd164ed6
EmadZandWeb3 <emadzandweb3@gmail.com>
2025-05-28 14:53:47 +0330

    Fibu

---
+++ Practices-Serie-2/fibonacci-sequence/fibonacci-sequence/Program.cs
@@ -0,0 +1,18 @@
+﻿int FibonacciFast(int n)
+{
+    return FibHelper(n, 0, 1);
+}
+
+int FibHelper(int n, int a, int b)
+{
+    if (n == 0)
+        return a;
+    if (n == 1)
+        return b;
+    return FibHelper(n - 1, b, a + b);
+}
+
+Console.Write("Enter n: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Fibonacci(" + n + ") = " + FibonacciFast(n));
